3507098841fac5b5fb341afc31d2c1655b808fa6
Juan Jose Alejo <juanjose.alejofalcon@nttdata.com>
2022-04-13 15:47:14 -0500

    Update - Reglas de Negocio

--- src/main/java/com/bootcamp/retailclient/RetailclientApplication.java
+++ src/main/java/com/bootcamp/retailclient/RetailclientApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RetailclientApplication {
 
 	public static void main(String[] args) {
+
+		System.setProperty("jdk.tls.client.protocols","TLSv1.2");
 		SpringApplication.run(RetailclientApplication.class, args);
 	}
 
