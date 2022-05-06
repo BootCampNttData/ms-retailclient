9c3b0fd0c17165648207ca94c369b9e6bc8b9174
Juan Jose Alejo <juanjose.alejofalcon@nttdata.com>
2022-05-06 13:08:38 -0500

    Azure

--- src/main/java/com/bootcamp/retailclient/controller/RetailClientController.java
+++ src/main/java/com/bootcamp/retailclient/controller/RetailClientController.java
@@ -45,7 +45,7 @@ public class RetailClientController {
     @PostMapping
     public Mono<RetailClient> create(@RequestBody RetailClient retailClient){
         return Mono.just(retailClient)
-                .then(check(retailClient, sav -> Optional.of(sav).isEmpty(), "Retail Client has not data"))
+                .then(check(retailClient, sav -> Optional.of(sav).isEmpty(), "Retail Client has not data.!!!"))
                 .then(check(retailClient, sav -> ObjectUtils.isEmpty(sav.getDocumentId()), "Client Id is required"))
                 .then(findByDocumentId(retailClient.getDocumentId())
                         .<RetailClient>handle((record, sink) -> sink.error(new RetailClientValidationException("The Client already Exist")))
